2fb15310aaed290e274e25b4f83c56be827b66a8
Nathan-M-Lee <lee.nathan.m@gmail.com>
2021-09-30 15:08:24 -0700

    created spring boot and packages

---
+++ Hierophant/src/main/java/com/hierophant/Hierophant/HierophantApplication.java
@@ -0,0 +1,13 @@
+package com.hierophant.Hierophant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierophantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HierophantApplication.class, args);
+	}
+
+}
---
+++ Hierophant/src/test/java/com/hierophant/Hierophant/HierophantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hierophant.Hierophant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HierophantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
