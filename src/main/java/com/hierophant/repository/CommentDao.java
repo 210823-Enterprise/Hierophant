7f51df57aed9cea610eaa7cc6e54c94e9d31b2b5
Nathan-M-Lee <lee.nathan.m@gmail.com>
2021-10-03 17:18:33 -0700

    added remaining crud methods to repository package

--- src/main/java/com/hierophant/repository/CommentDao.java
+++ src/main/java/com/hierophant/repository/CommentDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hierophant.model.Comment;
+import com.hierophant.model.Post;
 
 @Repository
 @Transactional
@@ -19,4 +20,13 @@ public interface CommentDao extends JpaRepository<Comment, Integer> {
 	// finds all the comments from a specific user
 //	public List<Comment> findByUsername(String userName);
 	public List<Comment> findByUserId(int userId);
+	
+	// inserts a comment to the database
+	public Comment insert(Comment comment);
+		
+	// updates a comment in the database (edit)
+	public boolean update(Comment comment);
+		
+	// deletes a comment from the database
+	public boolean deleteById(int id);
 }
--- src/main/java/com/hierophant/repository/ImageDao.java
+++ src/main/java/com/hierophant/repository/ImageDao.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hierophant.model.Comment;
 import com.hierophant.model.Image;
 
 @Repository
@@ -12,4 +13,13 @@ public interface ImageDao extends JpaRepository<Image, Integer>{
 	
 	// finds an image based on its id
 	public Optional<Image> findById(int id);
+	
+	// inserts a image to the database
+	public Comment insert(Image image);
+			
+	// updates a image in the database (edit)
+	public boolean update(Image image);
+			
+	// deletes a image from the database
+	public boolean deleteById(int id);
 }
